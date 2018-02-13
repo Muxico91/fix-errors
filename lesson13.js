1b35b4a6149d09428d08a0b3401ca6e0d76aad6e
Peter Horton <phorton@softstackfactory.org>
2018-02-13 04:42:10 +0000

    we were pushing filteredName i times into array as oppoed to currentName, which would change depending on the value of i and whether or not the name cleared our if check.

--- lesson13.js
+++ lesson13.js
@@ -8,15 +8,14 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
-
+];
 
 var nameFilter = function(filteredName) {
     var results = []
     for (var i = 0; i < people.length; i++) {
         let currentName = people[i].name
         if (currentName !== filteredName) {
-            results.push(filteredName)
+            results.push(currentName)
         }
     }
     return results
