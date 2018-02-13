17facbe8a55e62956a02a6a7e1c8b4c8aaaed447
Peter Horton <phorton@softstackfactory.org>
2018-02-13 05:21:08 +0000

    added name into console log for the fat arrow function, then passed in i as the argument when inviked within nameFilter function

--- lesson16.js
+++ lesson16.js
@@ -12,7 +12,7 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+]
 
 
 var nameFilter = function(filteredName, anonymousFunction) {
@@ -20,6 +20,7 @@ var nameFilter = function(filteredName, anonymousFunction) {
     for (var i = 0; i < people.length; i++) {
         let currentName = people[i].name
         if (currentName != filteredName) {
+            anonymousFunction(i);
             results.push(currentName)
 
         }
@@ -28,7 +29,7 @@ var nameFilter = function(filteredName, anonymousFunction) {
     return results
 }
 
-var filteredNames = nameFilter("Betty", function(name) {
-    console.log("This Ran")
+var filteredNames = nameFilter("Betty", (name) => {
+    console.log("This Ran " + name);
 })
 console.log(filteredNames)
